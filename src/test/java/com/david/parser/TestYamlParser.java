d4e89ebd48b92939a000ab7624693f901060d8e6
David <davidh3@gmail.com>
2021-10-05 19:20:32 +0200

    [YAML PARSER] improve unitary test for parsing

--- src/test/java/com/david/parser/TestYamlParser.java
+++ src/test/java/com/david/parser/TestYamlParser.java
@@ -1,9 +1,14 @@
 package com.david.parser;
 
 import com.david.parser.bean.Info;
+import org.junit.Assert;
 import org.junit.Test;
 
 import java.io.File;
+import java.util.Arrays;
+import java.util.List;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertEquals;
 
 
 public class TestYamlParser {
@@ -15,9 +20,27 @@ public class TestYamlParser {
     }
 
     @Test
-    public void parseYamlFile() throws Exception {
+    public void parseYamlFileShouldSuccess() throws Exception {
         Parser parser = new YamlParser();
         Info info = parser.read(new File("src/test/resources/file2.yaml"), Info.class);
+        assertNotNull(info);
+        assertEquals("dakar",info.getName());
+        assertEquals("Une bibliothèque qui permet de créer des éléments à collectionner.",info.getDescription());
+        assertEquals("0.0.1",info.getVersion());
+        assertEquals("any",info.getDependencies().getArgs());
+        assertEquals("any",info.getDependencies().getBrowser());
+        assertEquals("any",info.getDependencies().getGeo());
+        assertEquals("any",info.getDependencies().getShelf());
+        assertEquals("any",info.getDependencies().getShelf_web_socket());
+        assertEquals("any",info.getDependencies().getShelf_static());
+        assertEquals("any",info.getDependencies().getXml_rpc());
+        assertEquals("^0.1.0",info.getDependencies().getDart_to_js_script_rewriter());
+        assertEquals("^3.1.0",info.getDependencies().getGoogle_maps());
+        assertEquals("any",info.getDev_dependencies().getTest());
+        List<String> transformers = info.getTransformers();
+        assertNotNull(transformers);
+        List<String> transformersWaited = Arrays.asList("dart_to_js_script_rewriter","js_anonymizer","js_compresser");
+        assertEquals(transformersWaited,transformers);
     }
 
 
